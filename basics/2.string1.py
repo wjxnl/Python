351d7463c690ccf622f7d064c9d86cb7370e0416
wjxl <079bei008@ioepc.np>
2024-01-09 19:47:55 +0545

    strings

---
+++ basics/2.string1.py
@@ -0,0 +1,14 @@
+course1 = 'Pythons course for beginners.' #It displays as it is.
+print(course1)
+course2= "Python's course for beginners." #for displaying quotation marks.  
+print(course2)
+course3= 'Python course for "beginners".' #for displaying double quotation mark
+print(course3)
+#for multiple lines of strings
+course4= '''
+Python
+Course
+for 
+Beginners.
+'''
+print(course4)
