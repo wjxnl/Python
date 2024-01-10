0e1009c49a5fc9d61be69a3ef970149898ebcaa2
wjxl <079bei008@ioepc.np>
2024-01-10 07:53:07 +0545

    updated

---
+++ basics/17.lists.py
@@ -0,0 +1,8 @@
+names = ['evelyn','erica', 'emily', 'fred' ]
+print(names)
+print(names[3]) #we can access individual element using an index.
+print(names[-1])
+print(names[2:])
+print(names[:])
+names[1] = 'ERICA'
+print(names[:])
---
+++ basics/18.largest_number.py
@@ -0,0 +1,7 @@
+numbers = [2,9,5,6,8]
+max = numbers[0] #assume max is number of first index
+for item in numbers:
+    if item > max:
+        max = item
+print(max)
+
