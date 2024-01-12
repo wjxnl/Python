c6bd91a0e4843cbb1de6580168b8432c6090d13b
wjxl <079bei008@ioepc.np>
2024-01-12 23:47:16 +0545

    updated

---
+++ 2_functions/01.basic.py
@@ -0,0 +1,6 @@
+def greet_user(): #defining a function
+    print('Hi, there!') #this will only get executed when function is called.
+    print('Welcome') 
+print('start')
+greet_user() #calling a function
+print('Finish')
