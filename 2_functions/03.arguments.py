21edab5f7d17137438c77144257118ebf2d8fd56
wjxl <079bei008@ioepc.np>
2024-02-25 18:55:45 +0545

    updated

--- 2_functions/03.arguments.py
+++ 2_functions/03.arguments.py
@@ -4,4 +4,6 @@ def greet_user(first_name, last_name): #two parameters
 print('start')
 greet_user("John", "Smith") #positional arguments
 greet_user(last_name ="Smith", first_name ="Amy") #keyword arguments
+greet_user("kim", last_name ="hyeri") # to mix positional and keyword arguments, we should always use positional arguments first and then keyword arguments
 print('Finish')
+
